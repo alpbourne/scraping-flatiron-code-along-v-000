2ec0a3dc303d5cd18297be45637c4fcb5df8bc25
alpbourne <alpbourne@gmail.com>
2017-10-04 19:06:41 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,10 +17,11 @@ class Scraper
   end
 
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    binding.pry
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      binding.pry
   end
-
+ 
 end
-
+ 
 Scraper.new.get_page
